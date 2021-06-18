650c1de9fbd2eafbd1fd2305664687b8573d1fa7
Giovanny Granda <giovanotti1906@hotmail.com>
2021-06-17 20:41:36 -0500

    server finished

---
+++ src/test/java/com/aprendizaje/eurekaserver/MicroservicesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendizaje.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
